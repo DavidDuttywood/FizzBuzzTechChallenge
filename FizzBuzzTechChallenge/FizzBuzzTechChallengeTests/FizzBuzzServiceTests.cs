7fb10943add3a1813e3d6ea1a796c390f79a77da
David Woodward <david.woodward@smoothwall.com>
2024-03-20 23:12:36 +0000

    Add test coverage for fizzbuzz outcomes

--- FizzBuzzTechChallenge/FizzBuzzTechChallengeTests/FizzBuzzServiceTests.cs
+++ FizzBuzzTechChallenge/FizzBuzzTechChallengeTests/FizzBuzzServiceTests.cs
@@ -17,5 +17,26 @@ namespace FizzBuzzTechChallengeTests
             var result = _fizzBuzzService.ResolveValue(1);
             Assert.Equal("1", result);
         }
+
+        [Fact]
+        public void FizzBuzzService_FactorsOf3ShouldReturnFizz()
+        {
+            var result = _fizzBuzzService.ResolveValue(3);
+            Assert.Equal("Fizz", result);
+        }
+
+        [Fact]
+        public void FizzBuzzService_FactorsOf5ShouldReturnBuzz()
+        {
+            var result = _fizzBuzzService.ResolveValue(5);
+            Assert.Equal("Buzz", result);
+        }
+
+        [Fact]
+        public void FizzBuzzService_FactorsOf15ShouldReturnFizzBuzz()
+        {
+            var result = _fizzBuzzService.ResolveValue(15);
+            Assert.Equal("Buzz", result);
+        }
     }
 }
